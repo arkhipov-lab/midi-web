552552ca79e4db1288213082eb6dc21320e777a1
Andrey <aarkhipov@authoritysoftware.com>
2026-03-24 00:45:17 +0100

    feat: added MidiInputSelect

---
+++ src/features/ui/MidiInputSelect/MidiInputSelect.styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import {Select, Space} from 'antd'
+
+export const MidiInputSelect = styled(Space)`
+  width: 100%;
+`
+
+export const MidiInputSelectComponent = styled(Select)`
+  width: 22.5rem;
+`
