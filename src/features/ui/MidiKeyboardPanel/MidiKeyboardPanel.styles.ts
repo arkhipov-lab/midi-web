b885f401eec5a2202fb098066f5003536a3796a8
Andrey <aarkhipov@authoritysoftware.com>
2026-03-24 03:17:57 +0100

    feat: added MidiKeyboardPanel

---
+++ src/features/ui/MidiKeyboardPanel/MidiKeyboardPanel.styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+import {Space} from 'antd'
+
+export const MidiKeyboardPanel = styled(Space)`
+  display: flex;
+  gap: 1rem;
+  align-items: stretch;
+  width: 100%;
+`
+
+export const MidiKeyboardMidiKeyboardContainer = styled(Space)`
+  flex: 1;
+  min-width: 0;
+`
